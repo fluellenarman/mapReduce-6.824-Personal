7edac38ee7247144752d900ccee50d6408788f25
Fluellen Arman Umali <fluellenarman@gmail.com>
2023-07-21 15:22:20 +0100

    FAU, reducer finished. TODO, cleanup code, have mapreduce take arguments, create a large .txt file to test on

--- main.go
+++ main.go
@@ -4,17 +4,20 @@ import (
 	"fmt"
 	mapReduce "here/localMapReduce"
 	"os"
+	"time"
 )
 
 func main() {
 	fmt.Println(os.Args[1])
+	startTime := time.Now()
 	if os.Args[1] == "sequential" {
 		// fmt.Println("here")
 		SerialWordCounter()
 	} else if os.Args[1] == "concurrent" {
 		ConcCoordinator()
-	} else {
+	} else if os.Args[1] == "mapreduce" {
 		mapReduce.Coordinator()
 	}
-
+	elapsedTime := time.Since(startTime)
+	fmt.Println("Time taken:", elapsedTime)
 }
